6d30eca2d368e859c6923af87ec6f244e1e20f7a
KromaStone <groot707cod@gmail.com>
2024-12-04 11:22:01 +0530

    resume

--- src/components/ContactAnimatedBeam.jsx
+++ src/components/ContactAnimatedBeam.jsx
@@ -54,7 +54,7 @@ export function ContactAnimatedBeam() {
                         </a>
                     </Circle>
                     <Circle ref={div4Ref} className="size-16 ">
-                        <a target="_blank" href="https://drive.google.com/file/d/1NMuv6Hamb0HaagNHqCQC4xmbutmLAJXn/view?usp=sharing" className="flex items-center justify-center w-auto size-16 text-xl font-bold px-3 text-[#121212] bg-[linear-gradient(#fff_50%,_#121212_50%)] bg-[length:100%_200%] bg-[position:0%_0%] rounded-lg shadow-md transition-[background-position,color] duration-500 hover:bg-[position:0%_100%] hover:text-white z-40">
+                        <a target="_blank" href="https://docs.google.com/document/d/1IjZnOTCmfhNEVtAqP9IHvY60gOz1fCLMS8Y3InCr4lI/edit?usp=sharing" className="flex items-center justify-center w-auto size-16 text-xl font-bold px-3 text-[#121212] bg-[linear-gradient(#fff_50%,_#121212_50%)] bg-[length:100%_200%] bg-[position:0%_0%] rounded-lg shadow-md transition-[background-position,color] duration-500 hover:bg-[position:0%_100%] hover:text-white z-40">
                             <button className="">Resume</button>
                         </a>
                     </Circle>
